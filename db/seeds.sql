23e16843d215a0de732d6e9933e6d1b7ac2bedba
Janelle Cikanek <janellecikanek@gmail.com>
2022-11-18 18:50:39 -0600

    schema complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,31 +10,17 @@ VALUES (1, Sales Lead, 100000, 1),
 (3, Lead Engineer, 150000, 2),
 (4, Software Engineer, 120000, 2), 
 (5, Account Manager, 160000, 3),
-(6, Accountant, 125000, F3), 
+(6, Accountant, 125000, 3), 
 (7, Legal Team Lead, 250000, 4),
 (8, Lawyer, 190000, 4);
 
 INSERT INTO employee (id, first_name, Last_name, role_id, manager_id)
-VALUES  (1, John, Doe, null),
-(2, Mike, Chan, John Doe),
-(3, Ashley, Rodriguez, null),
-(4, Kavin, Tupik, Ashley Rodriguez), 
-(5, Kunal, Singh, null),
-(6, Malia, Brown, Kunal Singh), 
-(7, Sarah, Lourd, null),
-(8, Tom, Allen, Sarah Lourd);
+VALUES  (1, John, Doe, 1, null),
+(2, Mike, Chan, 2, 1),
+(3, Ashley, Rodriguez, 3, null),
+(4, Kavin, Tupik, 4, 3), 
+(5, Kunal, Singh, 5, null),
+(6, Malia, Brown, 6, 5), 
+(7, Sarah, Lourd, 7 ,null),
+(8, Tom, Allen, 8, 7);
 
-
-
-
-CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT, 
-    manager_id INT 
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-
-
-need to join with title , department, salary, 
