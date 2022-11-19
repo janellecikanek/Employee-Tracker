cd005e4795399f3beef45166ce9011708efcf971
Janelle Cikanek <janellecikanek@gmail.com>
2022-11-18 21:47:12 -0600

    starter code for index.js

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
@@ -22,7 +22,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT, 
-    manager_id INT 
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+//REQUIRE SCHEMA?
 //let teamArr = []
 
 const optionPrompt = () => {
@@ -12,22 +12,111 @@ const optionPrompt = () => {
     choices: ["View all departments", "View all roles", 'View all employees', 'Add a department', "Add a role", "Add an employee", "Update an employee role"]
 }
  ])
+//******SHOW DEPTARTMENT TABLE{
+when: (input) => input.options === "View all departments"
+},
 
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: "Team manager's name?",
-//     },
-//     {
-//       type: 'input',
-//       name: 'id',
-//       message: "Team manager's employee ID?",
-//     },
-//     {
-//       type: 'input',
-//       name: 'email',
-//       message: "Team manager's email",
-//     },
+//****SHOW ROLE TABLE{
+  when: (input) => input.options === "View all roles"
+},
+
+//******SHOW JOINED TABLE{
+  {
+    type: 'input',
+    name: 'school',
+    message: "What is the intern's school?",
+    when: (input) => input.role === "Intern"
+  },
+
+when: (input) => input.options === "View all employees"
+},
+const departmentPrompt = () => {
+    return inquirer.prompt([
+{
+  type: 'input',
+  name: 'deptName',
+  message: "What is the name of the department?",
+  when: (input) => input.options === "Add a department"
+},
+//*********Add department to database{
+
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+//********Add role to database
+const rolePrompt = () => {
+  return inquirer.prompt([
+
+{
+  type: 'input',
+  name: 'roleName',
+  message: "What is the name of the role?",
+  when: (input) => input.options === "Add a role"
+},
+{
+  type: 'input',
+  name: 'roleSalary',
+  message: "What is the salary for this role?",
+  when: (input) => input.options === "Add a role"
+},
+{
+  type: 'input',
+  name: 'roleDepartment',
+  message: "What department is this role in?",
+  when: (input) => input.options === "Add a role"
+},
+
+//WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+//**********Add employee to database{
+
+  const employeePrompt = () => {
+    return inquirer.prompt(
+    type: 'input',
+    name: 'employeeFirstName',
+    message: "What is the first name of the employee?",
+  when: (input) => input.options === "Add an employee"
+},
+
+{
+  type: 'input',
+  name: 'employeeLastName',
+  message: "What is the last name of the employee?",
+when: (input) => input.options === "Add an employee"
+},
+
+{
+  type: 'input',
+  name: 'employeeRole',
+  message: "What is the role of the employee?",
+when: (input) => input.options === "Add an employee"
+},
+{
+  type: 'input',
+  name: 'employeeMgr',
+  message: "Who is the manager of the employee?",
+when: (input) => input.options === "Add an employee"
+},
+//****************Update Employee in database
+
+// 
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+const employeeRolePrompt = () => {
+  return inquirer.prompt(
+{
+  type: "list",
+  name: "employeeUpdate",
+  message: "What employee would you like to update?",
+  //choices: [****LIST ARRAY OF CURRENT EMPLOYEES]
+  when: (input) => input.options === "Update an employee role"
+},
+//********UPDATE DATABASE with updated employee
+
+
+// prompts.complete();
+
 //     {
 //       type: 'input',
 //       name: 'office',
@@ -39,41 +128,6 @@ const optionPrompt = () => {
 //       console.log(manager)
 //     })
 // }
-
-// const employeePrompt = () => {
-//   return inquirer.prompt([
-//     {
-//       type: "list",
-//       name: "role",
-//       message: "What is this employee's role?",
-//       choices: ["Engineer", "Intern"]
-//     },
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: "Employee's name?",
-//     },
-//     {
-//       type: 'input',
-//       name: 'id',
-//       message: "Employee's ID?",
-//     },
-//     {
-//       type: 'input',
-//       name: 'email',
-//       message: "Employee's email",
-//     },
-//     {
-//       type: 'input',
-//       name: 'school',
-//       message: "What is the intern's school?",
-//       when: (input) => input.role === "Intern"
-//     },
-//     {
-//       type: 'input',
-//       name: 'github',
-//       message: "What is the engineer's github?",
-//       when: (input) => input.role === "Engineer"
 //     },
 //     {
 //       type: "confirm",
