a15ad96a254248dea5dd182e3b92b4f84adf040b
Janelle Cikanek <janellecikanek@gmail.com>
2022-11-20 12:57:20 -0600

    working dept table

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,10 @@
-SELECT
-role.department_id AS name, department.id AS dept 
+SELECT *
+
 FROM employee
-JOIN department ON role.department_id = department.id
-table 3 employee id, first name, last name, title, department, salary, manager
+JOIN role ON role.id = role_id
+ON 
+
+
+//table 3 employee id, first name, last name, title, department, salary, manager
 
 
--- index.js
+++ index.js
@@ -1,150 +1,166 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const cTable = require('console.table')
+const db = require("./config/connection")
 
 const getEmp = () => {
   return db.promise().query('SELECT * FROM employee')
 }
 
-.then((data) => {
-  if (data.menu === "View all employees") {
-    getEmp().then((empData) => {
-      console.table(empData[0])
-      menu();
-    })
-  } else if (data.options === "Add an employee") {
-    return addEmp();
-  } else if (data.options === "Update an employee role") {
-    return updateRole();
-  }
 
-  const menu = () => {
-    return inquirer.prompt([
-      {
-        type: "list",
-        name: "options",
-        message: "What would you like to do?",
-        choices: ["View all departments", "View all roles", 'View all employees', 'Add a department', "Add a role", "Add an employee", "Update an employee role"]
-      }
-    ])
 
-    const getDept = () => {
-      return db.promise().query('Seclect * FROM employee')
-    }
-.then((data) => {
-      if (data.menu === "View all departments") {
-        getDept().then((deptData) => {
-          console.table(deptData[0])
-          menu();
-        })
-      }
-
-      //****SHOW ROLE TABLE{
-      when: (input) => input.options === "View all roles"
-    },
-
-      //******SHOW JOINED TABLE{
-      {
-        type: 'input',
-        name: 'school',
-        message: "What is the intern's school?",
-        when: (input) => input.role === "Intern"
-      },
-
-      when: (input) => input.options === "View all employees"
-},
-  let deptArr[]
-const departmentPrompt = () => {
+const menu = () => {
   return inquirer.prompt([
     {
-      type: 'input',
-      name: 'deptName',
-      message: "What is the name of the department?",
-      when: (input) => input.options === "Add a department"
-    },
+      type: "list",
+      name: "options",
+      message: "What would you like to do?",
+      choices: ["View all departments", "View all roles", 'View all employees', 'Add a department', "Add a role", "Add an employee", "Update an employee role"]
+    }
+  ]).then((data) => {
+    if (data.menu === "View all employees") {
+      getEmp().then((empData) => {
+        console.table(empData[0])
+        menu();
+      })
+    } else if (data.options === "Add an employee") {
+      return addEmp();
+    } else if (data.options === "Update an employee role") {
+      return updateRole();
+    } else if (data.options === "View all departments") {
+      getDept().then((deptData) => {
+        console.log(deptData)
+        console.table(deptData[0])
+        menu();
+      })
+    }else if (data.options === 'Add a department') {
+      addDept().then(deptData => {
+        console.log(deptData)
+
+        console.table(deptData[0])
+        menu();
+      }) .catch(error => console.log (error))
+  
+
+    }
+  })}
+const addDept = () => {
+  return db.promise().query('Select * FROM department')
+}
+
+
+const getDept = () => {
+  return db.promise().query('Select * FROM department')
+}
+// .then((data) => {
+//   if (data.menu === "View all departments") {
+
+//   }
+
+  //****SHOW ROLE TABLE{
+//   when: (input) => input.options === "View all roles"
+// },
+
+  //******SHOW JOINED TABLE{
+//   {
+//     type: 'input',
+//     name: 'school',
+//     message: "What is the intern's school?",
+//     when: (input) => input.role === "Intern"
+//   },
+
+//   when: (input) => input.options === "View all employees"
+// },
+//   let deptArr[]
+// const departmentPrompt = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'deptName',
+//       message: "What is the name of the department?",
+//       when: (input) => input.options === "Add a department"
+//     },
     //*********Add department to database{
-    UPDATE department 
-SET 
-WHERE
 
+    // WHEN I choose to add a role
+    // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+    //********Add role to database
 
-//********Add role to database
+//     let roleArr = []
+// const rolePrompt = () => {
+//     return inquirer.prompt([
 
-let roleArr = []
-const rolePrompt = () => {
-    return inquirer.prompt([
-
-      {
-        type: 'input',
-        name: 'roleName',
-        message: "What is the name of the role?",
-        when: (input) => input.options === "Add a role"
-      },
-      {
-        type: 'input',
-        name: 'roleSalary',
-        message: "What is the salary for this role?",
-        when: (input) => input.options === "Add a role"
-      },
-      {
-        type: 'input',
-        name: 'roleDepartment',
-        message: "What department is this role in?",
-        when: (input) => input.options === "Add a role"
-      },
+//       {
+//         type: 'input',
+//         name: 'roleName',
+//         message: "What is the name of the role?",
+//         when: (input) => input.options === "Add a role"
+//       },
+//       {
+//         type: 'input',
+//         name: 'roleSalary',
+//         message: "What is the salary for this role?",
+//         when: (input) => input.options === "Add a role"
+//       },
+//       {
+//         type: 'input',
+//         name: 'roleDepartment',
+//         message: "What department is this role in?",
+//         when: (input) => input.options === "Add a role"
+//       },
 
       //WHEN I choose to add an employee
       // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
       //**********Add employee to database{
-      Let employeeArr[]
+//       Let employeeArr[]
 
 
-  const employeePrompt = () => {
-      return inquirer.prompt(
-        type: 'input',
-        name: 'employeeFirstName',
-        message: "What is the first name of the employee?",
-        when: (input) => input.options === "Add an employee"
-},
+//   const employeePrompt = () => {
+//       return inquirer.prompt(
+//         type: 'input',
+//         name: 'employeeFirstName',
+//         message: "What is the first name of the employee?",
+//         when: (input) => input.options === "Add an employee"
+// },
 
-      {
-        type: 'input',
-        name: 'employeeLastName',
-        message: "What is the last name of the employee?",
-        when: (input) => input.options === "Add an employee"
-  },
+//       {
+//         type: 'input',
+//         name: 'employeeLastName',
+//         message: "What is the last name of the employee?",
+//         when: (input) => input.options === "Add an employee"
+//   },
 
-    {
-      type: 'input',
-      name: 'employeeRole',
-      message: "What is the role of the employee?",
-      when: (input) => input.options === "Add an employee"
-},
-  {
-    type: 'input',
-    name: 'employeeMgr',
-    message: "Who is the manager of the employee?",
-    when: (input) => input.options === "Add an employee"
-},
+//     {
+//       type: 'input',
+//       name: 'employeeRole',
+//       message: "What is the role of the employee?",
+//       when: (input) => input.options === "Add an employee"
+// },
+//   {
+//     type: 'input',
+//     name: 'employeeMgr',
+//     message: "Who is the manager of the employee?",
+//     when: (input) => input.options === "Add an employee"
+// },
 //****************Update Employee in database
 
 // 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-const employeeRolePrompt = () => {
-  return inquirer.prompt(
-    {
-      type: "list",
-      name: "employeeUpdate",
-      message: "What employee would you like to update?",
-      //choices: [****LIST ARRAY OF CURRENT EMPLOYEES]
-      when: (input) => input.options === "Update an employee role"
-    },
-    //********UPDATE DATABASE with updated employee
+// const employeeRolePrompt = () => {
+//   return inquirer.prompt(
+//     {
+//       type: "list",
+//       name: "employeeUpdate",
+//       message: "What employee would you like to update?",
+//       //choices: [****LIST ARRAY OF CURRENT EMPLOYEES]
+//       when: (input) => input.options === "Update an employee role"
+//     },
+//     //********UPDATE DATABASE with updated employee
 
 
-    menu.complete();
+//     menu.complete();
+
+menu();
 
